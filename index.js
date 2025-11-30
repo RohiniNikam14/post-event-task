53e4eb8d8004e1b9e90fea2de5d38f69033fab36
rohini nikam <rohinipatil0114@gmail.com>
2025-11-30 13:31:59 +0530

    update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/files', fileRoutes);
 
 app.get("/",(req,res) => {
-res.send('API is running... ');
+res.send('Studygeni running perfectly... ');
 });
 
 app.listen(PORT, () => {
